1d5b2872bc7180da59626e84832a9ead259e7232
Ryan <ryoung@brandeis.edu>
2016-03-21 17:59:53 -0400

    Moving files for python version to its own branch.

--- SingleProcess.py
+++
@@ -1,3 +0,0 @@
-# Imports the tool set that will enable us to walk through folders and manipulate files
-from os import os.path 		# crawling through folders
-from os import subprorcess 	# running commands
